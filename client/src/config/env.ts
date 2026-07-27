e1d4ba148743e4e5817bf67ca40d86b8d3423d72
Taher Maimoon <tmaimoon18@gmail.com>
2026-07-27 13:36:41 +0530

    feat: update Docker configurations and environment handling for client and server

--- client/src/config/env.ts
+++ client/src/config/env.ts
@@ -1,8 +1,7 @@
 import type { Config } from "@/types";
 
 const env = Object.freeze({
-//   API_URL: import.meta.env.VITE_API_URL || "http://localhost:4000/api",
-  API_URL: "http://localhost:4000/api",
+  API_URL: import.meta.env.VITE_API_URL,
 }) satisfies Config;
 
 
