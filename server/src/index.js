b7673b51999f7437a74e5c4e57bd44151723590d
Taher Maimoon <tmaimoon18@gmail.com>
2026-07-30 02:29:25 +0530

    fix: update server listening message for clarity

--- server/src/index.js
+++ server/src/index.js
@@ -2,5 +2,5 @@ import env from "./config/env.js";
 import app from "./app.js";
 
 app.listen(env.PORT, "0.0.0.0", () => {
-  console.log(`Server is running on: http://localhost:${env.PORT}`);
+  console.log(`Server is listening on 0.0.0.0:${env.PORT}`);
 });
